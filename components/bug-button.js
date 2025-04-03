d6224d37dea04ed2584c2dad65bda7af75f1ceb7
alvina-dr <alvinadr24@gmail.com>
2025-04-03 11:22:42 +0200

    Fix : button image size

--- components/bug-button.js
+++ components/bug-button.js
@@ -9,13 +9,14 @@ export default function BugButton({title, index, description, url}) {
             margin: "auto", 
             backgroundColor:"red", 
             width:"calc(100% - 20px)", 
-            height:"15vw", 
+            // height:"20vw", 
             margin:"10px", 
             alignItems:"center", 
-            borderRadius:"5px"}} >
-                <img src={url} style={{width:"100%", height:"10vw", position:"absolute", objectFit:"cover"}}/>
-            <p style={{fontWeight:"bold", margin:"10px", alignSelf:"flex-end", justifySelf:"flex-start"}}>{index}</p>
-            <p style={{fontWeight:"bold", marginTop:"7vw"}}>{title}</p>
+            borderRadius:"5px",
+            overflow:"hidden"}} >
+                <img src={url} style={{width:"100%", height:"100%",  objectFit:"cover"}}/>
+            <p style={{fontWeight:"bold", margin:"10px", alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}}>{index}</p>
+            <p style={{fontWeight:"bold", margin:"10px"}}>{title}</p>
             <p className="white">{description}</p>
         </div>
 
