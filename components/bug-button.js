0ee30137333249a8137aa0790c51da126c70936f
alvina-dr <alvinadr24@gmail.com>
2025-04-07 15:07:54 +0200

    Add : Can now open bug detail popup

--- components/bug-button.js
+++ components/bug-button.js
@@ -2,9 +2,25 @@ import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 
 export default function BugButton({title, index, description, url}) {
+function OpenBugDetail() 
+{
+    console.log("click button");
+    const elem = document.getElementById("bug-popup");
+    elem.style.display = "flex";
+
+    const bugImg = document.getElementById("bug-detail-img");
+    bugImg.src = url;
+    
+    const bugDescription = document.getElementById("bug-detail-description");
+    bugDescription.textContent = description;
+    
+    const bugLogs = document.getElementById("bug-detail-logs");
+    bugLogs.textContent = index;
+}
+
     return (
     <>
-    <button href="" 
+    <button onClick={OpenBugDetail}
     style={{display: "flex", 
             flexDirection : "column", 
             margin: "auto", 
@@ -15,7 +31,7 @@ export default function BugButton({title, index, description, url}) {
             border:"none", 
             borderRadius:"5px",
             overflow:"hidden"}} >
-                <img src={url} style={{width:"100%", height:"100%",  objectFit:"cover"}}/>
+            <img src={url} style={{width:"100%", height:"100%",  objectFit:"cover"}}/>
             <p style={{fontWeight:"bold", margin:"10px", alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}}>{index}</p>
             <p style={{fontWeight:"bold", margin:"10px"}}>{title}</p>
             <p>{description}</p>
@@ -23,4 +39,3 @@ export default function BugButton({title, index, description, url}) {
     </>
   );
 }
-
--- components/bug-detail-popup.js
+++ components/bug-detail-popup.js
@@ -1,7 +1,7 @@
 export default function BugDetailPopup() {
     return (
     <>
-        <div style={{display: "flex", 
+        <div id="bug-popup" style={{display: "flex", 
             flexDirection : "column", 
             margin: "auto", 
             backgroundColor:"blue", 
@@ -14,10 +14,20 @@ export default function BugDetailPopup() {
             zIndex:"500",
             position:"fixed",
             display:"none"}} >
+            <button style={{alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}} onClick={CloseBugDetail}>X</button>
             <h3>POPUP</h3>
+            <img id="bug-detail-img" src={""} style={{width:"100%", height:"50%",  objectFit:"cover"}}/>
+            <p id="bug-detail-description">description</p>
+            <p id="bug-detail-logs">logs</p>
         </div>
 
     </>
   );
+}
+
+function CloseBugDetail() 
+{
+    const elem = document.getElementById("bug-popup");
+    elem.style.display = "none";
 }
 
