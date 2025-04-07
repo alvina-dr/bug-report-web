c4ad38068689eaf18b39a33ed8692754ae027343
alvina-dr <alvinadr24@gmail.com>
2025-04-07 10:23:24 +0200

    Clean : Next homepage

--- components/bug-button.js
+++ components/bug-button.js
@@ -4,22 +4,22 @@ import Image from "next/image";
 export default function BugButton({title, index, description, url}) {
     return (
     <>
-        <div style={{display: "flex", 
+    <button href="" 
+    style={{display: "flex", 
             flexDirection : "column", 
             margin: "auto", 
             backgroundColor:"red", 
             width:"calc(100% - 20px)", 
-            // height:"20vw", 
             margin:"10px", 
-            alignItems:"center", 
+            alignItems:"center",
+            border:"none", 
             borderRadius:"5px",
             overflow:"hidden"}} >
                 <img src={url} style={{width:"100%", height:"100%",  objectFit:"cover"}}/>
             <p style={{fontWeight:"bold", margin:"10px", alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}}>{index}</p>
             <p style={{fontWeight:"bold", margin:"10px"}}>{title}</p>
-            <p className="white">{description}</p>
-        </div>
-
+            <p>{description}</p>
+    </button>
     </>
   );
 }
---
+++ components/bug-detail-popup.js
@@ -0,0 +1,23 @@
+export default function BugDetailPopup() {
+    return (
+    <>
+        <div style={{display: "flex", 
+            flexDirection : "column", 
+            margin: "auto", 
+            backgroundColor:"blue", 
+            width:"80%", 
+            height:"60vh", 
+            margin:"auto", 
+            alignItems:"center", 
+            borderRadius:"5px",
+            overflow:"hidden",
+            zIndex:"500",
+            position:"fixed",
+            display:"none"}} >
+            <h3>POPUP</h3>
+        </div>
+
+    </>
+  );
+}
+
