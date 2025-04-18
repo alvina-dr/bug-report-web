7bcc1be3adecfc14e7b60878961c3e965bf5e650
alvina-dr <alvinadr24@gmail.com>
2025-04-18 10:44:53 +0200

    Tweak : bug detail window now has scroll

--- components/bug-detail-popup.js
+++ components/bug-detail-popup.js
@@ -6,8 +6,8 @@ export default function BugDetailPopup() {
         <div id="bug-popup" className={styles.BugDetailPopup}>
             <button className={styles.BugDetailBackground} onClick={CloseBugDetail}></button>
             <div className={styles.BugDetail}>
-                <button style={{alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}} onClick={CloseBugDetail}>X</button>
-                <img id="bug-detail-img" src={""} style={{width:"100%", height:"50%",  objectFit:"cover"}}/>
+                {/* <button style={{alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}} onClick={CloseBugDetail}>X</button> */}
+                <img id="bug-detail-img" style={{width:"100%", height:"auto",  objectFit:"cover"}}/>
                 <p id="bug-detail-index" style={{fontWeight:"bold", margin:"10px", alignSelf:"flex-end", justifySelf:"flex-start", position:"absolute"}}>1</p>
                 <p className={styles.BugDetailName} id="bug-detail-name">name</p>
                 <p className={styles.BugDetailDescription} id="bug-detail-description">description</p>
